db515fbf7f28bac59738b1690a08f0bcde2e8aa2
Jeferson Luis <jgjefersonluis@gmail.com>
2023-02-21 03:31:55 -0300

    casting

---
+++ secao2/casting.py
@@ -0,0 +1,8 @@
+num1 = input('Digite o primeiro numero: ')
+num2 = input('Digite o segundo numero: ')
+
+print(f'A soma é: {num1 + num2}') # erro # concatenação
+
+print(f'A soma é: {int(num1) + int(num2)}')
+
+# Obs.: Casting - conversão de um tipo de dado para outro
