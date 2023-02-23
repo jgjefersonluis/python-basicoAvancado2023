7f0a0cd2ad6731f351857bda517c39c5c07d7e90
Jeferson Luis <jgjefersonluis@gmail.com>
2023-02-23 16:33:30 -0300

    funções print e input - casting - exercicios - dir e help

---
+++ secao2/03_exercicios/exercicio02_Input.py
@@ -0,0 +1,4 @@
+nome = input('Digite seu nome: ') #Recebendo o nome do usuário
+idade = input('Digite sua idade: ') #Recebendo a idade do usuário
+
+print(f'Seu nome é {nome}. E a sua idade é {idade} anos.')
---
+++ secao2/04_dir_Help/dir_Help.py
@@ -0,0 +1,18 @@
+"""
+Dir e Help
+Dir: Apresenta uma lista de funções e métodos que podem utilizados em algum tipo de dado.
+Sintaxe: dir(valor/tipo de dado)
+
+Help: Apresenta as intruções de utilização das funções e métodos disponíveis para determinado tipo
+      de dado.
+Sintaxe: help(função) ou help(valor/tipo de dado.método/função)
+"""
+
+#dir
+#print(dir('urso'))
+#print(dir(18))
+#print(dir(float))
+
+#help
+#print(help(print))
+print(help('dudu'.title))
